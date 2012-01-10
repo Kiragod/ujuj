7db0afc0574069d1694c4b7bbce8496598520375
Denis V. Bakaev <Kiragods@gmail.com>
2012-01-10 18:27:32 +0200

    df

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ujuj::Application.config.secret_token = 'a8a9a02f56052b09ff7cda011dfb5e062c8e1c7fc64dd404e26057be9e075e632b60347aea3c6ce8cfc8aa9b4b03d07c99809d65709701fc10a90ad8834667a4'
